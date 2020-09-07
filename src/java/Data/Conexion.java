cc761e71353a5cb1b55a0fee363352ab32cfaa8c
JEISSON <JEISSON@192.168.30.90>
2020-09-07 09:51:17 -0500

    Creación de la conexión

--- src/java/Data/Conexion.java
+++ src/java/Data/Conexion.java
@@ -15,7 +15,7 @@ public class Conexion {
     public DriverManagerDataSource conectar() {
         DriverManagerDataSource datasource = new DriverManagerDataSource();
         datasource.setDriverClassName("com.mysql.jdbc.Driver");
-        datasource.setUrl("jdbc:mysql://localhost:3306/ciudaddepto");
+        datasource.setUrl("jdbc:mysql://localhost:3306/articulos");
         datasource.setUsername("root");
         datasource.setPassword("");
         return datasource;
